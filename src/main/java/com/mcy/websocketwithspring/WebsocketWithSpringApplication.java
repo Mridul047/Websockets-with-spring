1f05d7539af8c32a0b80b7bd7a9c790f82b2a7dd
mridul <mridul.choudhary7@gmail.com>
2022-11-15 20:39:37 +0530

    Project setup

---
+++ src/main/java/com/mcy/websocketwithspring/WebsocketWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.mcy.websocketwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcy/websocketwithspring/WebsocketWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcy.websocketwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
